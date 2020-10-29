d504cb46a6f91e8c7f610afc4cb62779b5cd4561
mike hill <michael-hill@lambdastudents.com>
2020-10-29 13:00:54 -0400

    wip

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require("./api/server.js");
+require('dotenv').config()
 
-const port = 5000;
+const port = process.env.PORT || 6000;// ONLY LOCAL
+const secret  = process.env.SECRET_THING 
+console.log(port, secret)
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
